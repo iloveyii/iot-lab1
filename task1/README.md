b45df4c5467bb03776d813d74800d18e49a2c1dc
Hazrat Ali <iloveyii@yahoo.com>
2019-12-07 20:02:31 +0100

    Initial commit

---
+++ task1/README.md
@@ -0,0 +1,13 @@
+IOT Lab 01 - Task 1
+=====================================
+
+Turn on/off a smart plug wirelessly – A taste of IoT
+
+## Description
+using the button at the Nordic Thingy52 to turn on/off a HS100 smart
+plug.
+## Implementation
+  * Install Node.js at Pi
+  * Connect Nordic Thingy52 to Pi and read the sensor data. In this case, the data is a button press.
+  * Connect your Pi to HS100 using the example code from the Internet.
+  * Command HS100 through Pi according to the button press event.
---
+++ task5/server/mongo/index.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const mongoClient = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
+
+const mongo = {
+    dbname: 'sensor_data',
+    url: 'mongodb://localhost:27017',
+    mongoOptions: {useNewUrlParser: true},
+};
+let db = null;
+
+function connectMongo() {
+    return new Promise(function (resolve, reject) {
+        if(db) { return resolve(db);}
+        console.log(mongo);
+        mongoClient.connect(mongo.url, mongo.mongoOptions, (err, client) => {
+            if (err) {
+                reject(err)
+            } else {
+                console.log('Mongodb connected to : ' + mongo.dbname);
+                db = client.db(mongo.dbname);
+                resolve(db);
+            }
+        });
+    })
+}
+
+
+function insertIntoMongo(d) {
+    return new Promise(function (resolve, reject) {
+        d._id && delete d['_id'];
+        db.collection('nt52').insertOne(d, (err, result) => {
+            if (err) {
+                reject(err);
+                console.log(err);
+            } else {
+                console.log('Data inserted into mongo');
+                console.log();
+                d = {};
+                resolve(d);
+            }
+        });
+    });
+}
+
+
+
+
+
+function f(d) {
+    connectMongo()
+        .then(() => insertIntoMongo(d)
+            .then(()=> d={}));
+}
+
+module.exports = f;
+
+// RUN AS : node reset_mongo.js
+// db-migrate --config
