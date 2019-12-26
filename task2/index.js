ed0fff57962c8f3e65d7fc388f558888b1489f9a
Hazrat Ali <iloveyii@yahoo.com>
2019-12-26 19:57:54 +0100

    Added console

--- task2/index.js
+++ task2/index.js
@@ -1,20 +1,18 @@
-// init project
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 const app = express();
 
 app.use(bodyParser.json());
 
-// The test/setup endpoint
+// The endpoint for IFTTT trigger - YOUR_IP/api/v1/netio/1 for ON - YOUR_IP/api/v1/netio/1 for OFF
 app.get("/api/v1/netio/:status", (req, res) => {
-	console.log('Received IFTT request');
+	console.log('Received IFTTTn request');
     res.status(200).send({
         status: req.params.status,
         message: 'Plug switched ' + (req.params.status == 1 ? 'ON' : 'OFF')
     });
 });
 
-const listener = app.listen(80, function() {
+const listener = app.listen(8080, function() {
     console.log("Your app is listening on port " + listener.address().port);
 });
